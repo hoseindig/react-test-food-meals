a195434f49a36f9622275650fe4b0a66e867388d
hoseindig <hosein_dig@yahoo.com>
2022-02-23 17:34:22 +0330

    meals

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,17 @@
+import styles from "./Modal.module.css";
+
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+import React, { Fragment } from "react";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;

825d0437f8649a6249dfe250211f5d3fd51dc788
hoseindig <hosein_dig@yahoo.com>
2022-02-24 18:19:28 +0330

    setup modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from "react";
 import deskImage from "../../assets/desk-foods.jpg";
-import Button from './HeaderCartButton'
+import Button from "./HeaderCartButton";
 import styles from "./Header.module.scss";
 
-const Header = (props) => {
+const Header = ({ setCardModal }) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <Button></Button>
+        <Button onClick={() => setCardModal(true)}></Button>
       </header>
       <div>
         <img src={deskImage} alt="" className={styles["main-image"]} />
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
-import styles from './HeaderCartButton.module.css'
-const HeaderCartBotton = () => {
+import styles from "./HeaderCartButton.module.css";
+const HeaderCartBotton = ({ onClick }) => {
   return (
     <Fragment>
-      <button className={styles.button}>
-        <span className={styles.icon}><i className="fa fa-shopping-cart" aria-hidden="true"></i></span>
+      <button className={styles.button} onClick={onClick}>
+        <span className={styles.icon}>
+          <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+        </span>
         <span>Your Card</span>
         <span className={styles.badge}>3</span>
       </button>
